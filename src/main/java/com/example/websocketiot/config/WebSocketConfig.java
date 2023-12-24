7e580000fec684a13ab0f28e4da82910ab3336c5
taitq <trqt2003@gmail.com>
2023-12-25 01:23:54 +0700

    create socket message between node and gateway

--- src/main/java/com/example/websocketiot/config/WebSocketConfig.java
+++ src/main/java/com/example/websocketiot/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-endpoint").withSockJS();
+        registry.addEndpoint("/hello").withSockJS();
     }
 }
 
